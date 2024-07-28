66aeebb46802f57944322d0e0ae1d65f4f6db951
codedreammer <nkanal38@gmail.com>
2024-07-28 17:07:24 +0530

    add the examples

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ a = 33030
 b = 3303
 print("A") if a > b else print("=") if a == b else print("B")
 
-c = 9 if a>b else 0 
-print(c)
+v = 9 if a>b else 0 
+print(v)
+
+c = 300
+d = 301
+print("OK") if c>d else print("=") if c == d else print("DONE")
+
+f = 299 if c<d else 0 
+print(f)
+
